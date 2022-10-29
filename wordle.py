34bfa2fa01621bafed42fae657fa4e2f71f720c7
CostyRo <ioncotinosu12@gmail.com>
2022-10-29 22:27:45 +0300

    func module for wordle

--- wordle.py
+++ wordle.py
@@ -1,24 +1,8 @@
 from random import choice
 
-debug=True
+from wordle_func import *
 
-def receive_word():
-    return input("Write your guess: ").upper()
-
-def find_green_info(user_guess,guess):
-    return [i==j for i,j in zip(user_guess,guess)]
-
-def find_yellow_info(user_guess,guess):
-    return [(letter in guess,guess.remove(letter) if letter in guess else 0)[0] for letter in user_guess]
-
-def transform_info(booleans,value=2):
-    return [value if boolean else 0 for boolean in booleans]
-
-def combine_info(green_info,yellow_info):
-    return [max(i,j) if (green_info.count(2)<yellow_info.count(1)) or i else 0 for i,j in zip(green_info,yellow_info)]
-
-def convert_info(info):
-    return "".join("X" if i==2 else "|" if i==1 else "0" for i in info)
+debug=False
 
 def main():
     guess=choice(database)
@@ -29,7 +13,7 @@ def main():
         print(guess)
 
     while True:
-        print(f"Attempt {attempt}/infinity!")
+        print(f"Attempt {attempt}/∞!")
 
         user_guess=receive_word()
         while len(user_guess)!=5:
@@ -50,7 +34,9 @@ def main():
         if debug:
             print(transformed_green_info,transformed_yellow_info,sep="\n")
             print(combine_info(transformed_green_info,transformed_yellow_info))
-            print(converted_info)
+
+        print(converted_info)
+        print()
 
         if user_guess==guess:
             print(f"The word was {guess}. You guessed it in {attempt} attemps!\n")
---
+++ wordle_func.py
@@ -0,0 +1,17 @@
+def receive_word():
+    return input("Write your guess: ").upper()
+
+def find_green_info(user_guess,guess):
+    return [i==j for i,j in zip(user_guess,guess)]
+
+def find_yellow_info(user_guess,guess):
+    return [(letter in guess,guess.remove(letter) if letter in guess else 0)[0] for letter in user_guess]
+
+def transform_info(booleans,value=2):
+    return [value if boolean else 0 for boolean in booleans]
+
+def combine_info(green_info,yellow_info):
+    return [max(i,j) if (green_info.count(2)<yellow_info.count(1)) or i else 0 for i,j in zip(green_info,yellow_info)]
+
+def convert_info(info):
+    return "".join("X" if i==2 else "|" if i==1 else "0" for i in info)
