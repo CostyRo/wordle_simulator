bda6896bb2d09f40e1bd6bd29a93a5280ea2324b
CostyRo <ioncotinosu12@gmail.com>
2022-10-29 22:06:22 +0300

    convert info and refactoring

--- wordle.py
+++ wordle.py
@@ -2,11 +2,14 @@ from random import choice
 
 debug=True
 
+def receive_word():
+    return input("Write your guess: ").upper()
+
 def find_green_info(user_guess,guess):
-    return [i.upper()==j for i,j in zip(user_guess,guess)]
+    return [i==j for i,j in zip(user_guess,guess)]
 
 def find_yellow_info(user_guess,guess):
-    return [(letter.upper() in guess,guess.remove(letter.upper()) if letter.upper() in guess else 0)[0] for letter in user_guess]
+    return [(letter in guess,guess.remove(letter) if letter in guess else 0)[0] for letter in user_guess]
 
 def transform_info(booleans,value=2):
     return [value if boolean else 0 for boolean in booleans]
@@ -14,10 +17,12 @@ def transform_info(booleans,value=2):
 def combine_info(green_info,yellow_info):
     return [max(i,j) if (green_info.count(2)<yellow_info.count(1)) or i else 0 for i,j in zip(green_info,yellow_info)]
 
+def convert_info(info):
+    return "".join("X" if i==2 else "|" if i==1 else "0" for i in info)
+
 if __name__=="__main__":
     with open("database.txt","r") as f:
-        database=f.read().split("\n")[:-1]
-        guess=choice(database)
+        guess=choice(f.read().split("\n")[:-1])
 
     attempt=0
     guesses=[]
@@ -30,24 +35,27 @@ if __name__=="__main__":
     while True:
         print(f"Attempt {attempt}/infinity!")
 
-        user_guess=input("Write your guess: ")
+        user_guess=receive_word()
         while len(user_guess)!=5:
             print("Error!\nIncorrect size of your guess!")
-            user_guess=input("Write your guess: ")
+            user_guess=receive_word()
         while user_guess in guesses:
             print("Error!\nYou already tried this word!")
-            user_guess=input("Write your guess: ")
+            user_guess=receive_word()
 
         attempt+=1
         guesses.append(user_guess)
 
         user_guess_list,guess_list=list(user_guess),list(guess)
+        transformed_green_info,transformed_yellow_info=transform_info(find_green_info(user_guess_list,guess_list)),transform_info(find_yellow_info(user_guess_list,guess_list),1)
+        combined_info=combine_info(transformed_green_info,transformed_yellow_info)
+        converted_info=convert_info(combined_info)
 
         if debug:
-            transformed_green_info,transformed_yellow_info=transform_info(find_green_info(user_guess_list,guess_list)),transform_info(find_yellow_info(user_guess_list,guess_list),1)
             print(transformed_green_info,transformed_yellow_info,sep="\n")
             print(combine_info(transformed_green_info,transformed_yellow_info))
+            print(converted_info)
 
-        if user_guess.upper()==guess:
+        if user_guess==guess:
             print(f"The word was {guess}. You guessed it in {attempt} attemps!")
             quit()
